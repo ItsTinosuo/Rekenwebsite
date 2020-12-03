f0519419a289e0ac8014b4ffb54d255609915cc8
Valentino Thoonen <valentino.thoonen@rocdeleijgraaf.nl>
2020-12-03 09:37:43 +0100

    first commit

---
+++ Index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<link rel="stylesheet" href="Index.css">
+<title>Page Title</title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<style>
+body {
+  font-family: Arial, Helvetica, sans-serif;
+}
+</style>
+</head>
+<body>
+<div class="topnav">
+    <a class="active" href="#home">Home</a>
+    <a href="#sommen">Sommen</a>
+    <a href="#contact">Contact</a>
+    <a href="#foto's">Foto's</a>
+    <a href="#info">Info</a>
+</div>
+<h1>Basisschool De Splinter</h1>
+<p>Dit is de home page hier komt een kleine introductie over de school</p>
+
+</body>
+</html>
